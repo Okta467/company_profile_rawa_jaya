fb7763e905d78c2f0d3bcd3c9caba740caf060b6
okta467 <oktaalfiansyah@gmail.com>
2024-07-10 07:44:18 +0700

    Add feature pengajuan bantuan sosial in landing page

---
+++ bantuan_sosial_pengajuan_tambah.php
@@ -0,0 +1,62 @@
+<?php
+	session_start();
+	include_once 'config/connection.php';
+
+	// cek apakah tombol submit ditekan sebelum memproses
+	if (!isset($_POST['xsubmit'])) {
+		$_SESSION['msg'] = 'other_error';
+		echo "<meta http-equiv='refresh' content='0;bantuan_sosial_pengajuan.php'>";
+		return;
+	}
+
+    require_once 'vendor/htmlpurifier/HTMLPurifier.auto.php';
+
+    // to sanitize user input
+    $config   = HTMLPurifier_Config::createDefault();
+    $purifier = new HTMLPurifier($config);
+    
+    $nik = htmlspecialchars($purifier->purify($_POST['xnik']));
+    $tipe_bantuan = $_POST['xtipe_bantuan'];
+    $is_allowed_tipe_bantuan = in_array($tipe_bantuan, ['PKH', 'BLT', 'pendidikan']);
+
+    if (!$is_allowed_tipe_bantuan) {
+        $_SESSION['msg'] = 'Tipe bantuan tidak terdapat pada daftar!';
+        echo "<meta http-equiv='refresh' content='0;bantuan_sosial_pengajuan.php'>";
+        return;
+    }
+
+    $stmt_penduduk = mysqli_stmt_init($connection);
+
+    mysqli_stmt_prepare($stmt_penduduk, "SELECT id, nik FROM tbl_penduduk WHERE nik=?");
+    mysqli_stmt_bind_param($stmt_penduduk, 'i', $nik);
+    mysqli_stmt_execute($stmt_penduduk);
+
+    $result = mysqli_stmt_get_result($stmt_penduduk);
+    $penduduk = mysqli_fetch_assoc($result);
+
+    if (!$penduduk) {
+        $_SESSION['msg'] = 'NIK tidak ditemukan!';
+        echo "<meta http-equiv='refresh' content='0;bantuan_sosial_pengajuan.php'>";
+        return;
+    }
+
+    $stmt_insert = mysqli_stmt_init($connection);
+
+    $id_penduduk = $penduduk['id'];
+    $status_pengajuan = 'belum_diproses';
+
+    mysqli_stmt_prepare($stmt_insert, "INSERT INTO tbl_bantuan_sosial (id_penduduk, tipe_bantuan, status_pengajuan) VALUES (?, ?, ?)");
+    mysqli_stmt_bind_param($stmt_insert, 'iss', $id_penduduk, $tipe_bantuan, $status_pengajuan);
+
+    $insert = mysqli_stmt_execute($stmt_insert);
+
+    !$insert
+        ? $_SESSION['msg'] = mysqli_stmt_error($stmt_insert)
+        : $_SESSION['msg'] = 'save_success';
+    
+    mysqli_stmt_close($stmt_penduduk);
+    mysqli_stmt_close($stmt_insert);
+    mysqli_close($connection);
+
+    echo "<meta http-equiv='refresh' content='0;bantuan_sosial_pengajuan.php'>";
+?>
---
+++ get_bantuan_sosial_by_nik.php
@@ -0,0 +1,40 @@
+<?php
+    include_once 'config/connection.php';
+    
+    $nik = $_POST['nik'];
+
+    if (!$nik) {
+        echo json_encode(array());
+        mysqli_close($connection);
+        return;
+    }
+
+    $stmt = mysqli_stmt_init($connection);
+    $query =
+        "SELECT
+            a.id AS id_penduduk, a.nik, a.nama_lengkap, a.jk, a.tmp_lahir, a.tgl_lahir, a.warga_negara, a.agama, a.pekerjaan, a.alamat, a.email, a.status_validasi AS status_validasi_penduduk, a.keterangan_validasi AS keterangan_validasi_penduduk,
+            b.id AS id_kartu_keluarga, b.nomor_kk, b.nik_kepala_keluarga,
+            c.id AS id_bantuan_sosial, c.tipe_bantuan, c.status_pengajuan, c.keterangan_pengajuan, c.created_at AS tgl_pengajuan
+        FROM tbl_penduduk AS a
+        LEFT JOIN tbl_kartu_keluarga AS b
+            ON b.id = a.id_kartu_keluarga
+        LEFT JOIN tbl_bantuan_sosial AS c
+            ON a.id = c.id_penduduk
+        WHERE a.nik=?";
+
+    mysqli_stmt_prepare($stmt, $query);
+    mysqli_stmt_bind_param($stmt, 'i', $nik);
+    mysqli_stmt_execute($stmt);
+
+	$result = mysqli_stmt_get_result($stmt);
+
+    $bantuan_sosials = !$result
+        ? array()
+        : mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($connection);
+
+    echo json_encode($bantuan_sosials);
+    return;
+?>
