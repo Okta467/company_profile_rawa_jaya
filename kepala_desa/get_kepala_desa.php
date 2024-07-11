4e7ed19c50f8565b294ca72aa65db4438dcfb59a
okta467 <oktaalfiansyah@gmail.com>
2024-07-11 08:46:09 +0700

    Add feature profil in role kepala desa

---
+++ kepala_desa/get_kepala_desa.php
@@ -0,0 +1,52 @@
+<?php
+    include_once '../helpers/isAccessAllowedHelper.php';
+
+    // cek apakah user yang mengakses adalah kepala_desa?
+    if (!isAccessAllowed('kepala_desa')) {
+        session_destroy();
+        echo "<meta http-equiv='refresh' content='0;" . base_url_return('index.php?msg=other_error') . "'>";
+        return;
+    }
+
+    include_once '../config/connection.php';
+    
+    $id_kepala_desa = $_POST['id_kepala_desa'];
+
+    $stmt1 = mysqli_stmt_init($connection);
+    $query = 
+        "SELECT
+            a.id AS id_kepala_desa, a.nip, a.nama_kepala_desa, a.jk, a.alamat, a.tmp_lahir, a.tgl_lahir, a.tahun_ijazah,
+            b.id AS id_jabatan, b.nama_jabatan, 
+            c.id AS id_pangkat_golongan, c.nama_pangkat_golongan, c.tipe AS tipe_pangkat_golongan,
+            d.id AS id_pendidikan, d.nama_pendidikan,
+            e.id AS id_jurusan_pendidikan, e.nama_jurusan AS nama_jurusan_pendidikan,
+            f.id AS id_pengguna, f.username, f.hak_akses
+        FROM tbl_kepala_desa AS a
+        LEFT JOIN tbl_jabatan AS b
+            ON a.id_jabatan = b.id
+        LEFT JOIN tbl_pangkat_golongan AS c
+            ON a.id_pangkat_golongan = c.id
+        LEFT JOIN tbl_pendidikan AS d
+            ON a.id_pendidikan = d.id
+        LEFT JOIN tbl_jurusan_pendidikan AS e
+            ON a.id_jurusan_pendidikan = e.id
+        LEFT JOIN tbl_pengguna AS f
+            ON a.id_pengguna = f.id
+        WHERE a.id=?";
+
+    mysqli_stmt_prepare($stmt1, $query);
+    mysqli_stmt_bind_param($stmt1, 'i', $id_kepala_desa);
+    mysqli_stmt_execute($stmt1);
+
+	$result = mysqli_stmt_get_result($stmt1);
+
+    $kepala_desas = !$result
+        ? array()
+        : mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_stmt_close($stmt1);
+    mysqli_close($connection);
+
+    echo json_encode($kepala_desas);
+
+?>
---
+++ kepala_desa/profil_ubah.php
@@ -0,0 +1,117 @@
+<?php
+    include_once '../helpers/isAccessAllowedHelper.php';
+
+    // cek apakah user yang mengakses adalah kepala_desa?
+    if (!isAccessAllowed('kepala_desa')) {
+        session_destroy();
+        echo "<meta http-equiv='refresh' content='0;" . base_url_return('index.php?msg=other_error') . "'>";
+        return;
+    }
+
+    require_once '../vendor/htmlpurifier/HTMLPurifier.auto.php';
+    include_once '../config/connection.php';
+
+    // to sanitize user input
+    $config   = HTMLPurifier_Config::createDefault();
+    $purifier = new HTMLPurifier($config);
+    
+    $id_kepala_desa      = $_SESSION['id_kepala_desa'];
+    $id_pengguna         = $_SESSION['id_pengguna'];
+    $password            = $_POST['xpassword'] ? password_hash($_POST['xpassword'], PASSWORD_DEFAULT) : null;
+    $alamat              = htmlspecialchars($purifier->purify($_POST['xalamat']));
+    $tmp_lahir           = htmlspecialchars($purifier->purify($_POST['xtmp_lahir']));
+    $tgl_lahir           = $_POST['xtgl_lahir'];
+    $tahun_ijazah        = $_POST['xtahun_ijazah'];
+    $id_jabatan          = $_POST['xid_jabatan'];
+    $id_pangkat_golongan = $_POST['xid_pangkat_golongan'];
+    $id_pendidikan       = $_POST['xid_pendidikan'];
+    $id_jurusan          = $_POST['xid_jurusan'] ?? null;
+
+    // Turn off autocommit mode
+    mysqli_autocommit($connection, false);
+
+    // Initialize the success flag
+    $success = true;
+
+    // Begin the transaction
+    try {
+        // Pegawai statement preparation and execution
+        $stmt_kepala_desa  = mysqli_stmt_init($connection);
+        $query_kepala_desa = "UPDATE tbl_kepala_desa SET
+            id_jabatan = ?
+            , id_pangkat_golongan = ?
+            , id_pendidikan = ?
+            , id_jurusan_pendidikan = ?
+            , alamat = ?
+            , tmp_lahir = ?
+            , tgl_lahir = ?
+            , tahun_ijazah = ?
+        WHERE id = ?";
+        
+        if (!mysqli_stmt_prepare($stmt_kepala_desa, $query_kepala_desa)) {
+            $_SESSION['msg'] = 'Statement Pegawai preparation failed: ' . mysqli_stmt_error($stmt_kepala_desa);
+            echo "<meta http-equiv='refresh' content='0;profil.php?go=profil'>";
+            return;
+        }
+        
+        mysqli_stmt_bind_param($stmt_kepala_desa, 'iiiisssii', $id_jabatan, $id_pangkat_golongan, $id_pendidikan, $id_jurusan, $alamat, $tmp_lahir, $tgl_lahir, $tahun_ijazah, $id_kepala_desa);
+        
+        if (!mysqli_stmt_execute($stmt_kepala_desa)) {
+            $_SESSION['msg'] = 'Statement Pegawai preparation failed: ' . mysqli_stmt_error($stmt_kepala_desa);
+            echo "<meta http-equiv='refresh' content='0;profil.php?go=profil'>";
+            return;
+        }
+
+        if ($password) {
+            // Pengguna statement preparation and execution
+            $stmt_pengguna  = mysqli_stmt_init($connection);
+            $query_pengguna = "UPDATE tbl_pengguna SET password=? WHERE id=?";
+            
+            if (!mysqli_stmt_prepare($stmt_pengguna, $query_pengguna)) {
+                $_SESSION['msg'] = 'Statement Pengguna preparation failed: ' . mysqli_stmt_error($stmt_pengguna);
+                echo "<meta http-equiv='refresh' content='0;profil.php?go=profil'>";
+                return;
+            }
+            
+            mysqli_stmt_bind_param($stmt_pengguna, 'si', $password, $id_pengguna);
+            
+            if (!mysqli_stmt_execute($stmt_pengguna)) {
+                $_SESSION['msg'] = 'Statement Pengguna preparation failed: ' . mysqli_stmt_error($stmt_pengguna);
+                echo "<meta http-equiv='refresh' content='0;profil.php?go=profil'>";
+                return;
+            }
+        }
+
+        // Commit the transaction if all statements succeed
+        if (!mysqli_commit($connection)) {
+            $_SESSION['msg'] = 'Transaction commit failed: ' . mysqli_stmt_error($stmt_pengguna);
+            echo "<meta http-equiv='refresh' content='0;profil.php?go=profil'>";
+            return;
+        }
+
+    } catch (Exception $e) {
+        // Roll back the transaction if any statement fails
+        $success = false;
+        mysqli_rollback($connection);
+        $_SESSION['msg'] = 'Transaction failed: ' . $e->getMessage();
+    }
+
+    !$success
+        ? ''
+        : $_SESSION['msg'] = 'save_success';
+
+    // Close the statements
+    mysqli_stmt_close($stmt_kepala_desa);
+
+    !$password
+        ? ''
+        : mysqli_stmt_close($stmt_pengguna);
+
+    // Turn autocommit mode back on
+    mysqli_autocommit($connection, true);
+
+    // Close the connection
+    mysqli_close($connection);
+
+    echo "<meta http-equiv='refresh' content='0;profil.php?go=profil'>";
+?>
